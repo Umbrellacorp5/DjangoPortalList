6c504abcfe7a27d9ce17aea18050c35989a22b25
portalList <umbrellacorpinc5@gmail.com>
2022-10-06 17:08:02 -0300

    url import from dot

--- PortalList/profesores/urls.py
+++ PortalList/profesores/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from alumnos import views
+from profesores import views
+from administracion import views
 
 urlpatterns = [
     path('elegirUsuario/', views.elegirUsuario),
