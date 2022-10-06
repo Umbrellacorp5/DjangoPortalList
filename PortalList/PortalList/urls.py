361ff1367674aba517ced03ae15ce476a387aa08
portalList <umbrellacorpinc5@gmail.com>
2022-10-06 17:10:36 -0300

    main

--- PortalList/PortalList/urls.py
+++ PortalList/PortalList/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from alumnos import views
+from profesores import views
+from administracion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
