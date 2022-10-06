3a5ccb169e8e417910a439e40c2043ca58d28542
umbrella <umbrellacorpinc5@gmail.com>
2022-10-06 12:07:35 -0300

    deployment fail

--- PortalList/administracion/models.py
+++ PortalList/administracion/models.py
@@ -1,12 +1,12 @@
 from enum import unique
 from django.db import models
 from alumnos.models import models
-from profesores.models import models
+from profesores.models import Lista
 # Create your models here.
 #crea las tablas en pyhton
 
 class Administrador(models.Model):
-    cod_administrador = models.IntegerField('cod_administrador', primary_key=True, null=False, unique=True)
+    codAdministrador = models.IntegerField( primary_key=True, null=False, unique=True)
     email = models.CharField(max_length=255, null=False)
     contraseña = models.CharField(max_length=255, null=False)
 
@@ -16,23 +16,23 @@ class Usuario(models.Model):
     contraseña = models.CharField(max_length=255, null=False)
     apellido = models.CharField(max_length=255, null=False)
     email = models.CharField(max_length=255, null=False)
-    cod_administrador = models.ForeignKey('cod_dministrador', on_delete=models.CASCADE, null=False)
+    codAdministrador = models.ForeignKey(Administrador,on_delete=models.CASCADE, null=False)
 
 
 class Grupo(models.Model):
-    cod_grupo = models.IntegerField("cod_grupo",primary_key=True, null=False)
+    codGrupo = models.IntegerField(primary_key=True, null=False)
     nombre = models.CharField(max_length=255, null=False)
 
 class Estan(models.Model):
-    cod_grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, primary_key=True, null=False)
+    codGrupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, primary_key=True, null=False)
     usuarioci = models.ForeignKey(Usuario, on_delete=models.CASCADE, primary_key=True, null=False)
 
 class Tienen(models.Model):
-    cod_grupo = models.ForeignKey("cod_grupo", on_delete=models.CASCADE, primary_key=True, null=False)
+    codGrupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, primary_key=True, null=False)
     usuarioci = models.ForeignKey(Usuario, on_delete=models.CASCADE, primary_key=True, null=False)
 
 class Pasan(models.Model):
-    cod_grupo = models.ForeignKey("cod_grupo", on_delete=models.CASCADE, primary_key=True, null=False)
+    codGrupo = models.ForeignKey( Grupo,on_delete=models.CASCADE, primary_key=True, null=False)
     usuarioci = models.ForeignKey(Usuario, on_delete=models.CASCADE, primary_key=True, null=False)
-    cod_lista = models.ForeignKey("cod_grupo", on_delete=models.CASCADE, primary_key=True, null=False)
+    cod_lista = models.ForeignKey(Lista,on_delete=models.CASCADE, primary_key=True, null=False)
     fecha = models.DateField(null=False)
--- PortalList/alumnos/models.py
+++ PortalList/alumnos/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from administracion.models import Usuario
-from profesores.models import *
 
 # Create your models here.
 
 
 class Alumno(models.Model):
     cod_alumno = models.IntegerField('cod_alumno',primary_key=True, null=False)
-    usuarioci = models.ForeignKey('administracion.Usuario', on_delete=models.CASCADE, primary_key=True, null=False)
+    usuarioci = models.ForeignKey(Usuario, on_delete=models.CASCADE, primary_key=True, null=False)
     num_padre = models.CharField('num_padre', max_length=255, null=False)
     foto = models.CharField(max_length=255)
     mac = models.CharField(max_length=255)
