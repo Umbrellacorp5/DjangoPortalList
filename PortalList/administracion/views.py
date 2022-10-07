9edea0c68bf18ed93af414d2afc3c36c2d970d32
portalList <umbrellacorpinc5@gmail.com>
2022-10-07 11:12:09 -0300

    prueba

--- PortalList/administracion/views.py
+++ PortalList/administracion/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .forms import RegistroAlumno
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 #Definir ue ejecutar y que enviar al cliente, enviar html
+
+def admin(request):
+    return render(request, admin.html)
