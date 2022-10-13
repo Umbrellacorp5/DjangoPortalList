5720e929cf432bc5c56f3c777b1eb8a91b01acdd
umbrella <umbrellacorpinc5@gmail.com>
2022-10-13 13:05:10 -0300

    ALGO

--- PortalList/administracion/views.py
+++ PortalList/administracion/views.py
@@ -31,7 +31,9 @@ import io
 from django.db import connections
 import os
 from django.db.models import Avg
-
+from decorators import allowed_users, unauthenticated_user,allowed_users_homeç
+from administracion.forms import *
+from administracion.forms import TeacherForm
 
 # Create your views here.
 #Definir ue ejecutar y que enviar al cliente, enviar html
@@ -42,7 +44,7 @@ def admin(request):
     
 
     #-------------------------------------------------------LogIn Administraciòn---------------------------------------------------
-
+@unauthenticated_user
 
 def ingresarAdministracion(request):
     if request.method == 'POST':
@@ -59,3 +61,27 @@ def ingresarAdministracion(request):
         
 
     return render(request, 'ingresarAdministracion/admin.html')
+
+
+#--------------------------------------------Register Profesor---------------------------------
+@login_required(login_url = 'registroProfesor')
+@allowed_users(allowed_roles=['admin'])
+def registroProfesor(request):
+    user_form = CreateUserForm(request.POST or None) 
+    teacher_form = TeacherForm(request.POST or None, request.FILES or None)
+    context = {'teacher_form': teacher_form,'user_form':user_form, 'page_title':'add student'}
+    if request.method == 'POST':
+        if user_form.is_valid and teacher_form.is_valid():
+            user = user_form.save()
+            teacher = teacher_form.save()
+            teacher.user =user
+            teacher.save()
+           
+            group = Group.objects.get(name = 'teacher')
+            user.groups.add(group)
+            messages.success(request, "Successfully Teacher Added")
+        else:
+            messages.success(request, "Teacher Couldn't  Added")
+            
+            
+    return render(request, 'registration_template/add_teacher.html',context)
