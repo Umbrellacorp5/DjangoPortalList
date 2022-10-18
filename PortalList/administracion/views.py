ec6a8e131b523d6e7306b3ed22dcba3af63dd6b0
PortalList <umbrellacorpinc5@gmail.com>
2022-10-18 08:26:16 +0200

    views

--- PortalList/administracion/views.py
+++ PortalList/administracion/views.py
@@ -111,6 +111,42 @@ def registroProfesor(request):
             
     return render(request, 'registration_template/add_teacher.html',context)
 
+    @login_required(login_url = 'login')
+@allowed_users(allowed_roles=['admin'])
+def registerPage(request):
+    user_form = CreateUserForm(request.POST or None) 
+    admin_form = AdminForm(request.POST or None, request.FILES or None)
+    context = {'admin_form': admin_form,'user_form':user_form, 'page_title':'add student'}
+    if request.method == 'POST':
+        if user_form.is_valid and admin_form.is_valid():
+            user = user_form.save()
+            admin = admin_form.save()
+            admin.user =user
+            admin.save()
+            # username = student_form.cleaned_data.get('username')
+            # email = student_form.cleaned_data.get('email')
+            # password1 = student_form.cleaned_data.get('password1')
+            # password2 = student_form.cleaned_data.get('password2')
+            # name = student_form.cleaned_data.get('name')
+            # name = student_form.cleaned_data.get('phone')
+            # passport = request.FILES['profile_pic']
+            # fs = FileSystemStorage()
+            # filename = fs.save(passport.name, passport)
+            # passport_url = fs.url(filename)
+            group = Group.objects.get(name = 'admin')
+            user.groups.add(group)
+        else:
+            messages.success(request, "Successfully Admin Added")
+    return render(request, 'registration_template/add_admin.html',context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login') 
+
+##-----------------------------------------------LOG IN END -------------------------------------------------------###
+
+
+
 
 
 
