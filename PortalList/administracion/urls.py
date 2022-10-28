7e466e8703eb6753aa95ac1ca134bc2f9d7e7730
portalList <umbrellacorpinc5@gmail.com>
2022-10-28 16:41:00 -0300

    rutas

--- PortalList/administracion/urls.py
+++ PortalList/administracion/urls.py
@@ -4,12 +4,12 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('contactUs/', views.contactUs, name='contactUs'),
-    path('ingresarAdministracion/', views.ingresarAdministracion, name='ingresarAdministracion'),
-    path('registroAlumno/', views.registroAlumno, name='registroAlumno'),
-    path ('elegirAdmin/', views.elegirAdmin, name='elegirAdmin'),
-    path('registroProfesor/', views.registroProfesor, name='registroProfesor'),
-    path("seleccionarRegistro/", views.seleccionarRegistro, name='seleccionarRegistro'),    
+    path('contactUs.html', views.contactUs, name='contactUs'),
+    path('ingresarAdministracion.html', views.ingresarAdministracion, name='ingresarAdministracion'),
+    path('registroAlumno.html', views.registroAlumno, name='registroAlumno'),
+    path('elegirAdmin.html', views.elegirAdmin, name='elegirAdmin'),
+    path('registroProfesor.html', views.registroProfesor, name='registroProfesor'),
+    path("seleccionarRegistro.html", views.seleccionarRegistro, name='seleccionarRegistro'),    
     path('',views.index, name='index'),
 
 ]  
