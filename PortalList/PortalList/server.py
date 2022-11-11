9adf4a6291986c1e387c1e3b053fa4cff3d692ed
admin <youremail@yourdomain.com>
2022-11-11 11:07:32 -0300

    server

---
+++ PortalList/PortalList/server.py
@@ -0,0 +1,25 @@
+from time import sleep
+import paramiko
+# Inicia un cliente SSH
+dirip = input('ingrese direcciòn ip del dispositivo a configurar')
+usssh = input('ingrese usuario SSH')
+clavessh = input('ingrese password SSH')
+port= 22
+
+ssh= paramiko.SSHClient()
+# Establecer política por defecto para localizar la llave del host localmente
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Conectarse
+ssh.connect(hostname=dirip,username=usssh, password=clavessh, port=port, look_for_keys=False, allow_agent=False)
+#ssh_client.connect(dirip, 22, usssh, clavessh)
+# Ejecutar un comando de forma remota capturando entrada, salida y error estándar
+
+comando1 = input('ingrese el comando')
+#for i in "ROBOTICA":
+
+   # texto= "mkdir "+ i+ ".txt"
+   # print(texto)
+
+stdin, stdout, stderr = ssh.exec_command(comando1)
+output = stdout.readlines()
+print(' '.join(map(str, output)))
