27fc802ca9e70e80392f5209938b9091058829ee
portalList <umbrellacorpinc5@gmail.com>
2022-11-18 04:30:43 -0300

    profileIMG

--- PortalList/PortalList/urls.py
+++ PortalList/PortalList/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,3 +23,6 @@ urlpatterns = [
     path('profesores/', include('profesores.urls')),
     path('alumnos/', include('alumnos.urls')),
 ]
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
--- PortalList/alumnos/models.py
+++ PortalList/alumnos/models.py
@@ -6,7 +6,7 @@ class Alumno(models.Model):
     codAlumno = models.IntegerField(primary_key=True, null=False)
     usuarioci = models.ForeignKey(to='administracion.Usuario', on_delete=models.CASCADE, null=False)
     numPadre = models.CharField(max_length=255, null=False)
-    fotoAlumno = models.ImageField(upload_to="Alumno")
+    fotoAlumno = models.ImageField(upload_to='profile_images', default='blank-profile-picture.jpg')
     mac = models.CharField(max_length=255)
 
     def __int__(self):
